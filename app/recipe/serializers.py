28d27b25fafd293ff56f28b3d24a4087e57644c5
Byran <byran.taljaard@gmail.com>
2024-11-24 19:42:12 +0100

    add the recipe app and create model, serializers, and views

---
+++ app/recipe/serializers.py
@@ -0,0 +1,17 @@
+"""
+Serializers for Recipe APIs
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Recipe model
+    """
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
